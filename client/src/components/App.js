59be7a43785347107e570b48b94f907263e7fe78
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 12:58:20 -0400

    add return statement to App to remove npm server launch error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+  return(
+    <>
+    Hello! Welcome! 
+    </>
+  )
 }
 
 export default App;
