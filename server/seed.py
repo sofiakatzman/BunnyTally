58f50baee7ad0e39737c2eb2e886f51ea1389fe9
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 14:41:13 -0400

    create seed data

--- server/seed.py
+++ server/seed.py
@@ -5,13 +5,76 @@ from random import randint, choice as rc
 
 # Remote library imports
 from faker import Faker
+from sqlalchemy import func
 
 # Local imports
 from app import app
-from models import db
+from config import db
+from models import Walk, Bunny, Path
+
+
+fake = Faker()
+
+def create_fake_walk():
+    date = fake.date()
+    start_time = fake.time()
+    end_time = fake.time()
+    bunny_count = fake.random_int(min=1, max=10)
+    walk_path = Path.query.order_by(func.random()).first().id
+
+    walk = Walk(
+        date=date,
+        start_time=start_time,
+        end_time=end_time,
+        bunny_count=bunny_count,
+        walk_path=walk_path,
+    )
+
+    return walk
+
+def create_fake_bunny():
+    name = fake.first_name()
+    description = fake.sentence()
+    headshot = fake.image_url()
+
+    bunny = Bunny(
+        name=name,
+        description=description,
+        headshot=headshot,
+    )
+
+    return bunny
+
+def create_fake_path():
+    name = fake.word()
+    directions = fake.paragraph(nb_sentences=2)
+
+    path = Path(
+        name=name,
+        directions=directions,
+    )
+
+    return path
+
+def generate_fake_data():
+    for _ in range(10):
+        path = create_fake_path()
+        db.session.add(path)
+    
+    for _ in range(20):
+        bunny = create_fake_bunny()
+        db.session.add(bunny)
+
+    for _ in range(50):
+        walk = create_fake_walk()
+        db.session.add(walk)
+
+    db.session.commit()
+
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+        generate_fake_data()
+        print("... seed complete")
