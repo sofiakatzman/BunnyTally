d97370d460b63c8a5a7b175357c9b7ced01ed07b
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 19:05:48 -0400

    create router & routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom" // Import Routes
+
+import Home from "./Home"
+import Navigation from "./Navigation"
+import Walks from "./Walks"
+import Bunnies from "./Bunnies"
+import Paths from "./Paths"
+import CreatePath from "./CreatePath"
+import CreateBunny from "./CreateBunny"
 
 function App() {
-  return(
+  return (
     <>
-    Hello! Welcome! 
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/walks" element={<Walks />} />
+          <Route path="/bunnies" element={<Bunnies />} />
+          <Route path="/paths" element={<Paths />} />
+          <Route path="/create/path" element={<CreatePath />} />
+          <Route path="/create/bunny" element={<CreateBunny />} />
+        </Routes>
+      </Router>
     </>
   )
 }
 
-export default App;
+export default App
