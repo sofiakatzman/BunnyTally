85a17877564a6930afea844e7ae39ac0c27c05c2
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 19:25:42 -0400

    add path delete button

--- client/src/components/Paths.js
+++ client/src/components/Paths.js
@@ -10,6 +10,23 @@ function Paths(){
         console.log(paths)
     }, [])
 
+    const handleDelete = (deleteID) => {
+        fetch(`/paths/${deleteID}`, {
+          method: "DELETE",
+        })
+          .then((response) => {
+            if (response.status === 204) {
+              console.log("Path deleted successfully!")
+              setPaths((prevPaths) => prevPaths.filter((path) => path.id !== deleteID))
+            } else {
+              console.error("Failed to delete bunny.")
+            }
+          })
+          .catch((error) => {
+            console.error("Error while deleting bunny:", error)
+          })
+      }
+
     return(
         <>
         {paths && paths.map(path=> {
@@ -17,6 +34,7 @@ function Paths(){
                 <div key={path.id}>
                     <h2>{path.name}</h2>
                     <h3>{path.directions}</h3>
+                    <button onClick={() => handleDelete(path.id)}>delete</button>
                 </div>
             )
         })}
