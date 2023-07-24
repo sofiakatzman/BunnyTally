229d4e3429e0dc769184d39ecafee20e73e7dc50
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 16:16:35 -0400

    add delete method for apis

--- server/app.py
+++ server/app.py
@@ -24,16 +24,43 @@ class Walks(Resource):
             200
         )
         return response
-    
-    # add a walk - TBC
+
+    # add a walk 
     def post(self):
-        pass
+        form_json = request.get_json()
+        new_walk = Walk(
+            date = form_json['date'],
+            start_time = form_json['start_time'],
+            end_time = form_json['end_time'],
+            bunny_count = form_json['bunny_count'],
+            walk_path = form_json['walk_path'],
+        )
+
+        db.session.add(new_walk)
+        db.session.commit()
+
+        response = make_response(
+            new_walk.to_dict(), 
+            201
+        )
+
+        return response
 
     # delete a walk - TBC
-    def delete(self):
-        pass
+    def delete(self, walk_id):
+        walk = Walk.query.filter_by(id=walk_id).first()
+        db.session.delete(walk)
+        db.session.commit()
 
-api.add_resource(Walks, '/walks')
+        response = make_response(
+            "deletion completed",
+            204
+        )
+
+        return response
+    
+api.add_resource(Walks, '/walks/', endpoint='walks')
+api.add_resource(Walks, '/walks/<int:walk_id>/', endpoint='walks_by_id')
 
 class Bunnies(Resource):
     # view all bunnies
@@ -45,15 +72,40 @@ class Bunnies(Resource):
         )
         return response
     
-    # add a bunny - TBC
+    # add a bunny 
     def post(self):
-        pass
+        form_json = request.get_json()
+        new_bunny = Bunny(
+            name = form_json['name'],
+            description = form_json['description'],
+            headshot = form_json['headshot']               
+        )
+        db.session.add(new_bunny)
+        db.session.commit()
+
+        response = make_response(
+            new_bunny.to_dict(),
+            201
+        )
+
+        return response
+
 
     # delete a bunny - TBC
-    def delete(self):
-        pass
+    def delete(self, bunny_id):
+        bunny = Bunny.query.filter_by(id=bunny_id).first()
+        db.session.delete(bunny)
+        db.session.commit()
 
-api.add_resource(Bunnies, '/bunnies')
+        response = make_response(
+            "deletion completed", 
+            204
+        )
+
+        return response
+    
+api.add_resource(Bunnies, '/bunnies', endpoint='bunnies')
+api.add_resource(Bunnies, '/bunnies/<int:bunny_id>/', endpoint='bunnies_by_id')
 
 class Paths(Resource):
     # view all paths
@@ -67,10 +119,38 @@ class Paths(Resource):
         return response
     
     # add a path 
+    def post(self):
+        form_json = request.get_json()
+        new_path = Path(
+            name=form_json['name'],
+            directions=form_json['directions']
+        )
+        db.session.add(new_path)
+        db.session.commit()
+
+        response = make_response(
+            new_path.to_dict(), 
+            201
+        )
+
+        return response
+    
     # delete a path 
+    def delete(self, path_id):
+        path = Path.query.filter_by(id=path_id).first()
+        db.session.delete(path)
+        db.session.commit()
 
-api.add_resource(Paths, '/paths')
+        response = make_response(
+            "deletion completed", 
+            204
+        )
 
+        return response
+
+
+api.add_resource(Paths, '/paths', endpoint="paths")
+api.add_resource(Paths, '/paths/<int:path_id>/', endpoint="path_by_id")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
