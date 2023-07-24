d36cdfc3e3a01de02d55010ce63ec135fcda1d60
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 19:28:31 -0400

    add walks delete button and function

--- client/src/components/Walks.js
+++ client/src/components/Walks.js
@@ -11,6 +11,23 @@ function Walks(){
     }, [])
 
 
+    const handleDelete = (deleteID) => {
+        fetch(`/walks/${deleteID}`, {
+          method: "DELETE",
+        })
+          .then((response) => {
+            if (response.status === 204) {
+              console.log("Path deleted successfully!")
+              setWalks((prevWalks) => prevWalks.filter((walk) => walk.id !== deleteID))
+            } else {
+              console.error("Failed to delete walk.")
+            }
+          })
+          .catch((error) => {
+            console.error("Error while deleting walk:", error)
+          })
+      }
+
     return(
         <>
         {walks && walks.map((walk)=> {
@@ -19,6 +36,7 @@ function Walks(){
                     <h2>{walk.date} : {walk.bunny_count}</h2>
                     <h3>Walk Time: {walk.start_time} - {walk.end_time}</h3>
                     <h3>Walk Path: {walk.walk_path}</h3>
+                    <button onClick={() => handleDelete(walk.id)}>delete</button>
                 </div>
             )
         })}
