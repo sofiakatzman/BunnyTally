563305dad56aac930b514b5a4642be7d7745ffd9
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 19:04:29 -0400

    create navigation

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from 'react-router-dom' 
+
+function Navigation(){
+    return(
+        <>
+        <Link to="/">home</Link><br/>
+        <Link to="/walks">walks</Link><br/>
+        <Link to="/bunnies">bunnies</Link><br/>
+        <Link to="/paths">paths</Link><br/><br/>
+        <Link to="/create/bunny">new bunny</Link><br/>
+        <Link to="/create/path">new path</Link>
+        </>
+    )
+}
+
+export default Navigation
