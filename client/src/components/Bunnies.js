d36010a50f673d45629f497c8150a5be2d8e88be
Sofia Katzman <katzmansof@gmail.com>
2023-07-24 19:05:35 -0400

    display bunnies

---
+++ client/src/components/Bunnies.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react"
+
+function Bunnies() {
+  const [bunnies, setBunnies] = useState(null)
+
+  useEffect(() => {
+    fetch("/bunnies")
+      .then((r) => r.json())
+      .then((data) => setBunnies(data))
+  }, [])
+
+  const handleDelete = (deleteID) => {
+    fetch(`/bunnies/${deleteID}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          console.log("Bunny deleted successfully!")
+          setBunnies((prevBunnies) => prevBunnies.filter((bunny) => bunny.id !== deleteID))
+        } else {
+          console.error("Failed to delete bunny.")
+        }
+      })
+      .catch((error) => {
+        console.error("Error while deleting bunny:", error)
+      })
+  }
+
+  return (
+    <>
+      {bunnies &&
+        bunnies.map((bunny) => (
+          <div key={bunny.id}>
+            <h1>{bunny.name}</h1>
+            <h2>{bunny.description}</h2>
+            <img src={bunny.headshot} alt={`Headshot of ${bunny.name}`} />
+            <button onClick={() => handleDelete(bunny.id)}>delete</button>
+          </div>
+        ))}
+    </>
+  )
+}
+
+export default Bunnies
